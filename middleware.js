3ad36944a53cedd7caa4cb1149dfcad5c82dd7a1
28raina <rainasaini98@gmail.com>
2023-11-09 21:19:20 +0530

    add file

--- middleware.js
+++ middleware.js
@@ -34,7 +34,7 @@ next();
 module.exports.isOwner = async (req,res,next)=>{
   try{let {id} = req.params;
       let listing = await Listing.findById(id);
-      if(!listing.owner.equals(res.locals.currUser._id)){             
+      if(!listing.owner._id.equals(res.locals.currUser._id)){             
           req.flash("error","You are not the owner of this listing");
           return res.redirect(`/listings/${id}`);
       }
